672d8e758cf560dedae5028b40d4e5b744e2d660
ChiHoc <genioschen@gmail.com>
2017-07-13 19:41:48 +0800

    Add Memcached enabled property.

--- src/main/java/org/mybatis/caches/memcached/MemcachedConfigurationBuilder.java
+++ src/main/java/org/mybatis/caches/memcached/MemcachedConfigurationBuilder.java
@@ -63,7 +63,7 @@ final class MemcachedConfigurationBuilder {
     settersRegistry.add(new BooleanPropertySetter("org.mybatis.caches.memcached.asyncget", "usingAsyncGet", false));
     settersRegistry
         .add(new BooleanPropertySetter("org.mybatis.caches.memcached.compression", "compressionEnabled", false));
-    settersRegistry.add(new BooleanPropertySetter("org.mybatis.caches.memcached.enable", "enabled", true));
+    settersRegistry.add(new BooleanPropertySetter("org.mybatis.caches.memcached.enabled", "enabled", true));
 
     settersRegistry.add(new InetSocketAddressListPropertySetter());
     settersRegistry.add(new ConnectionFactorySetter());
